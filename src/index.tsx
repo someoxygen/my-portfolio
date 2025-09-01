ec6ef53ce8dad368c8a2f1f90449742be31e7390
someoxygen <mustafaycl37@gmail.com>
2025-09-01 22:58:19 +0300

    predeployment2

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-
 root.render(
   <React.StrictMode>
-    {/* GitHub Pages alt dizini için basename belirt */}
-    <BrowserRouter basename="/my-portfolio">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
